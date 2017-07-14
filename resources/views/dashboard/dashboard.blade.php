c227ecd72994d0ad8b37bf2c7ddc2e3029a213d2
David Camhi <dcamhi@nearshoremx.com>
2017-07-14 17:54:48 -0500

    FIx links

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -35,46 +35,54 @@
 	    <!-- BEGIN DASHBOARD STATS 1-->
 	    <div class="row">
 	        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<div class="visual">
-					<i class="fa fa-comments"></i>
-				</div>
-				<div class="details">
-					<div class="number">
-						<span data-counter="counterup" data-value="{{ $total_products }}">0</span>
-					</div>
-					<div class="desc"> Productos </div>
-				</div>
+	            <a class="dashboard-stat dashboard-stat-v2 blue" href="#">
+	                <div class="visual">
+	                    <i class="fa fa-comments"></i>
+	                </div>
+	                <div class="details">
+	                    <div class="number">
+	                        <span data-counter="counterup" data-value="{{ $total_products }}">0</span>
+	                    </div>
+	                    <div class="desc"> Productos </div>
+	                </div>
+	            </a>
 	        </div>
 	        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<div class="visual">
-					<i class="fa fa-bar-chart-o"></i>
-				</div>
-				<div class="details">
-					<div class="number">
-						<span data-counter="counterup" data-value="{{ $total_colors }}">0</span> </div>
-					<div class="desc"> Colores </div>
-				</div>
+	            <a class="dashboard-stat dashboard-stat-v2 red" href="#">
+	                <div class="visual">
+	                    <i class="fa fa-bar-chart-o"></i>
+	                </div>
+	                <div class="details">
+	                    <div class="number">
+	                        <span data-counter="counterup" data-value="{{ $total_colors }}">0</span> </div>
+	                    <div class="desc"> Colores </div>
+	                </div>
+	            </a>
 	        </div>
 	        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<div class="visual">
-					<i class="fa fa-shopping-cart"></i>
-				</div>
-				<div class="details">
-					<div class="number">
-						<span data-counter="counterup" data-value="{{ $total_categories }}">0</span>
-					</div>
-					<div class="desc"> Líneas </div>
-				</div>
+	            <a class="dashboard-stat dashboard-stat-v2 green" href="#">
+	                <div class="visual">
+	                    <i class="fa fa-shopping-cart"></i>
+	                </div>
+	                <div class="details">
+	                    <div class="number">
+	                        <span data-counter="counterup" data-value="{{ $total_categories }}">0</span>
+	                    </div>
+	                    <div class="desc"> Líneas </div>
+	                </div>
+	            </a>
 	        </div>
 	        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<div class="visual">
-					<i class="fa fa-globe"></i>
-				</div>
-				<div class="details">
-					<div class="number">
-						<span data-counter="counterup" data-value="{{ $total_users }}">0</span> </div>
-					<div class="desc">Administradores </div>
-				</div>
+	            <a class="dashboard-stat dashboard-stat-v2 purple" href="#">
+	                <div class="visual">
+	                    <i class="fa fa-globe"></i>
+	                </div>
+	                <div class="details">
+	                    <div class="number">
+	                        <span data-counter="counterup" data-value="{{ $total_users }}">0</span> </div>
+	                    <div class="desc">Administradores </div>
+	                </div>
+	            </a>
 	        </div>
 	    </div>
 	    <div class="clearfix"></div>
